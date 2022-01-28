2bd881b4cbf02b7e6dbba291c4b05c679b415b8d
MicaPupparo <micaelapupparo@gmail.com>
2022-01-28 20:52:27 -0300

    agregué componente usuarios

--- Estructura/dashboard-react/src/components/secciones/Usuarios.jsx
+++ Estructura/dashboard-react/src/components/secciones/Usuarios.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react'
+import "./usuarios.css";
+import PropTypes from 'prop-types';
 
 
-function Usuarios(){
+ export default function Usuarios() {
+   const [usuarios, setUsuarios] = useState([]);
 
-  return(
-    <div>
-      <p>test de usuarios</p>
-    </div>
-  )
-};
+   useEffect(() => {
+     console.log('%cse monto el componente', 'color: green');
+     fetch('http://localhost:3000/api/users')
+        .then(response => response.json())
+        .then( data => {
+          setUsuarios(data)
+        })
+        .catch(error => console.error(error))
+   }, [])
 
-export default Usuarios;
+   useEffect(() => {
+    console.log('%cse actualizó el componente', 'color: orange');
+    }, [usuarios])
+
+    useEffect(() => {
+      return () => console.log('%cse desmontó el componente', 'color: red');
+      }, [])
+
+   return (
+     <div className="usuariosContainer">
+        <h2>Data Usuarios</h2>
+       <div className="totalUsuarios">
+         <p>TOTAL USUARIOS: {usuarios.count}</p>
+       </div>
+       <div className="todosLosUsuarios">
+         <ul>
+           { usuarios.length === 0 && <p>Cargando...</p> }
+           {
+             usuarios.users.map((usuario, i) => {
+               return (
+                 <li key={i}>
+                   <h3>{usuario.name}</h3>
+                   <img src={usuario.avatar} alt="avatar" width="150" />
+                 </li>
+               )
+             })
+           }
+         </ul>
+       </div> 
+     </div>
+   )
+ }
